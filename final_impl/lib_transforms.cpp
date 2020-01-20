50c6fa3133850bc9258ebefc9e3895a5012b58ae
zaied54 <55851339+zaied54@users.noreply.github.com>
2020-01-20 11:17:15 -0500

    Update lib_transforms.cpp

--- final_impl/lib_transforms.cpp
+++ final_impl/lib_transforms.cpp
@@ -1,23 +1,3 @@
-/*
- * Copyright (c) 2011, Marc Lebrun <marc.lebrun@cmla.ens-cachan.fr>
- * All rights reserved.
- *
- * This program is free software: you can use, modify and/or
- * redistribute it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later
- * version. You should have received a copy of this license along
- * this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-
-/**
- * @file lib_transforms.cpp
- * @brief 1D and 2D wavelet transforms
- *
- * @author Marc Lebrun <marc.lebrun@cmla.ens-cachan.fr>
- **/
-
 #include "lib_transforms.h"
 #include <math.h>
 
